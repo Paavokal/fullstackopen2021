b370fcc4b6b76b807163f131e3bdec54092a6f35
Paavokal <paavo.kalliala@gmail.com>
2021-12-07 13:07:26 +0200

    app.js

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,19 +1,8 @@
-require('dotenv').config()
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
-const notesRouter = require('./controllers/blogs')
 
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', notesRouter)
-
+const server = http.createServer(app)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
